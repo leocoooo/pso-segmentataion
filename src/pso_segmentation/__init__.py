c6105a856e39a0a8fb0ac1dbb3198b7bf6b4b57e
Colin <leocolin7002@gmail.com>
2026-05-30 12:38:18 +0200

    Fix public import ordering

--- src/pso_segmentation/__init__.py
+++ src/pso_segmentation/__init__.py
@@ -8,6 +8,13 @@ Version: 0.1.0
 """
 
 from pso_segmentation.api import segment_scores
+from pso_segmentation.io import (
+    export_metrics_to_json,
+    export_segmentation_to_csv,
+    import_segmentation_from_csv,
+    load_optimizer_state,
+    save_optimizer_state,
+)
 from pso_segmentation.objective_functions_examples import (
     example_fitness_custom_business_metric,
     example_fitness_gini_focused,
@@ -15,13 +22,6 @@ from pso_segmentation.objective_functions_examples import (
     example_fitness_r2_with_all_constraints,
     example_fitness_r2_with_balance_penalty,
     example_fitness_r2_with_monotonic_penalty,
-)
-from pso_segmentation.io import (
-    export_metrics_to_json,
-    export_segmentation_to_csv,
-    import_segmentation_from_csv,
-    load_optimizer_state,
-    save_optimizer_state,
 )
 from pso_segmentation.optimizer import OptimizerConfig, SegmentationOptimizer
 from pso_segmentation.segmentation import SegmentationResult
